1ed2e598b9a92f3bd61d2719546b788720a69a56
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-08-03 19:23:48 +0300

    finish lab07

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-const express = require('express');
+const express = require("express");
 const cors = require('cors');
 const app = express();
 const weather = require('./data/weather.json');
 app.use(cors());
 require('dotenv').config();
 
+// const cors = require('cors');
+
+class Forecast {
+  constructor(value) {
+    this.valid_date = value.valid_date;
+    this.description = ` ${value.weather.description}`;
+  }
+}
+
 app.get('/', (req, res) => {
   res.send(weather);
 });
-
-class Forecast {
-  constructor(data, description) {
-    this.data = data;
-    this.description = description;
-  }
-}
 app.get('/weather', (req, res) => {
-  let latitude = req.query.lat;
-  let longitude = req.query.lon;
-  let searchQuery = req.query.searchQuery;
 
-  const found = weather.find(element => element.lat === latitude && element.lon === longitude && element.city_name === searchQuery);
-  console.log(found);
-  const arrOfDays = [];
-  found.data.map(item => {
-    let descriptionDay = `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
-    let weatherDay = new Forecast(item.valid_date, descriptionDay);
-    arrOfDays.push(weatherDay);
-  });
-  res.send(arrOfDays);
+  try {
+    let { searchQuery, lat, lon } = req.query;
+    let findCity = weather.find(element =>
+      element.city_name.toLowerCase() === searchQuery.toLowerCase() ||
+      (element.lat === lat && element.lon === lon)
+    );
+    let forecastArr = findCity.data.map(items => new Forecast(items));
+    res.send(forecastArr);
+  }
+  catch (e) {
+    res.status(404).send('City not found. Please enter the correct city name');
+  }
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => console.log(`listening on ${process.env.PORT}`));
